46b69fbc205c448b2e5682ea1ac370583db4d03b
KingRainbow44 <kobedo11@gmail.com>
2024-10-02 14:00:00 +0000

    fix(WordHuntCommand.java): Fix button interaction

--- src/main/java/moe/seikimo/magixbot/commands/WordHuntCommand.java
+++ src/main/java/moe/seikimo/magixbot/commands/WordHuntCommand.java
@@ -4,11 +4,13 @@ import moe.seikimo.magixbot.utils.EmbedUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.interactions.InteractionContextType;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle;
 import tech.xigam.cch.command.Command;
 import tech.xigam.cch.command.modifiers.Arguments;
+import tech.xigam.cch.command.modifiers.Callable;
 import tech.xigam.cch.command.modifiers.Completable;
 import tech.xigam.cch.utils.Argument;
+import tech.xigam.cch.utils.Callback;
 import tech.xigam.cch.utils.Completion;
 import tech.xigam.cch.utils.Interaction;
 
@@ -16,7 +18,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public final class WordHuntCommand extends Command implements Arguments, Completable {
+public final class WordHuntCommand extends Command implements Arguments, Callable, Completable {
     public WordHuntCommand() {
         super("wordhunt", "Invite a player to play Word Hunt!", InteractionContextType.PRIVATE_CHANNEL);
     }
@@ -27,7 +29,7 @@ public final class WordHuntCommand extends Command implements Arguments, Complet
 
         var rules = "Time limit: 1m30s" +
                 "\n" +
-                "Game expires in " +
+                "Game expires " +
                 EmbedUtils.relativeTime(30, TimeUnit.MINUTES);
 
         var embed = new EmbedBuilder()
@@ -41,16 +43,27 @@ public final class WordHuntCommand extends Command implements Arguments, Complet
 
         interaction
                 .addButtons(
-                        Button.primary("join", "Game On!"))
+                        this.createButton(ButtonStyle.PRIMARY, "join", "Game On!"))
                 .reply(embed);
     }
 
     @Override
     public void complete(Completion completion) {
         completion
-                .addChoice("an-option", "The first option")
-                .addChoice("another-option", "The second option")
+                .addChoice("The first option", "an-option")
+                .addChoice("The second option", "another-option")
                 .reply();
+    }
+
+    @Override
+    public void callback(Callback callback) {
+        var user = callback.getUser();
+        callback.reply("Hello %s! (you clicked %s)"
+                .formatted(user.getAsMention(), callback.getReference()));
+
+        user.openPrivateChannel().queue(channel -> {
+            channel.sendMessage("Hello, you clicked the button earlier; hopefully one day there will be a Word Hunt board here...").queue();
+        });
     }
 
     @Override
