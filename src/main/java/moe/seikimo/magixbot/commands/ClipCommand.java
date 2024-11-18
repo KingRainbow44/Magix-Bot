c6e39c702303b8015aeae6b169a213907f48ff1e
KingRainbow44 <kobedo11@gmail.com>
2024-11-17 23:13:51 -0500

    feat(pom.xml): Upgrade to CCH `v1.9.0-SNAPSHOT`

--- src/main/java/moe/seikimo/magixbot/commands/ClipCommand.java
+++ src/main/java/moe/seikimo/magixbot/commands/ClipCommand.java
@@ -5,14 +5,14 @@ import moe.seikimo.magixbot.commands.clip.RecordSubCommand;
 import moe.seikimo.magixbot.commands.clip.StartSubCommand;
 import moe.seikimo.magixbot.commands.clip.StopSubCommand;
 import moe.seikimo.magixbot.utils.EmbedUtils;
+import net.dv8tion.jda.api.interactions.InteractionContextType;
 import tech.xigam.cch.command.Command;
 import tech.xigam.cch.command.modifiers.Baseless;
-import tech.xigam.cch.command.modifiers.Limited;
 import tech.xigam.cch.utils.Interaction;
 
-public final class ClipCommand extends Command implements Baseless, Limited {
+public final class ClipCommand extends Command implements Baseless {
     public ClipCommand() {
-        super("clip", "Record audio from a voice channel.");
+        super("clip", "Record audio from a voice channel.", InteractionContextType.GUILD);
 
         this.registerSubCommand(new StartSubCommand());
         this.registerSubCommand(new StopSubCommand());
@@ -23,10 +23,5 @@ public final class ClipCommand extends Command implements Baseless, Limited {
     @Override
     public void execute(Interaction interaction) {
         interaction.reply(EmbedUtils.info("Usage: `clip <start|stop|duration|record> [duration]`"));
-    }
-
-    @Override
-    public boolean isGuildOnly() {
-        return true;
     }
 }
--- src/main/java/moe/seikimo/magixbot/commands/GameCommand.java
+++ src/main/java/moe/seikimo/magixbot/commands/GameCommand.java
@@ -6,14 +6,14 @@ import moe.seikimo.magixbot.commands.game.StartSubCommand;
 import moe.seikimo.magixbot.commands.game.TerminateSubCommand;
 import moe.seikimo.magixbot.utils.EmbedUtils;
 import moe.seikimo.magixbot.utils.JDAUtils;
+import net.dv8tion.jda.api.interactions.InteractionContextType;
 import tech.xigam.cch.command.Command;
 import tech.xigam.cch.command.modifiers.Baseless;
-import tech.xigam.cch.command.modifiers.Limited;
 import tech.xigam.cch.utils.Interaction;
 
-public final class GameCommand extends Command implements Limited, Baseless {
+public final class GameCommand extends Command implements Baseless {
     public GameCommand() {
-        super("game", "Manage your chat games.");
+        super("game", "Manage your chat games.", InteractionContextType.GUILD);
 
         this.registerSubCommand(new CreateSubCommand());
         this.registerSubCommand(new StartSubCommand());
@@ -26,10 +26,5 @@ public final class GameCommand extends Command implements Limited, Baseless {
         if (JDAUtils.isNotFromGuild(interaction)) return;
 
         interaction.reply(EmbedUtils.info("Usage: `game <create|start|terminate|join> [game type|game id]`"));
-    }
-
-    @Override
-    public boolean isGuildOnly() {
-        return true;
     }
 }
